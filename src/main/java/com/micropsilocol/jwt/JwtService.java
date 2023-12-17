bbc70accd205ee2cc4207e305fb5829ffd357d97
MPorras01 <mauricioporras8@gmail.com>
2023-12-17 03:44:34 -0500

    feat: :recycle: Se refactoriza codigo para su correcto funcionamiento
    Se refactoriza codigo para su correcto funcionamiento

--- src/main/java/com/micropsilocol/jwt/JwtService.java
+++ src/main/java/com/micropsilocol/jwt/JwtService.java
@@ -15,7 +15,7 @@ import java.util.Map;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "123321";
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     public String getToken(UserDetails user) {
         return getToken(new HashMap<>(), user);
     }
@@ -27,12 +27,7 @@ public class JwtService {
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000*60*24))
-                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
                 .compact();
-    }
-
-    private Key getKey() {
-        byte[] KeyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(KeyBytes);
     }
 }
